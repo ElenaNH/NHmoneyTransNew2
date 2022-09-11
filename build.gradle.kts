eabb2b13d2ee918e1a6cc483a19ecee7e8a026dc
Elena Nefedova <elenanefedova@gmail.com>
2022-09-11 15:05:33 +0300

    Autotests after build.gradle.kts updating

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.10"
     application
+    id ("java")
+    id ("jacoco")
+
 }
 
 group = "ru.netology"
@@ -20,9 +23,10 @@ dependencies {
     testImplementation ("junit:junit:4.13.2")
 }
 
+/* // Вот это сказали удалить, иначе тесты не компилились. Откуда этот кусок кода взялся и зачем?
 tasks.test {
     useJUnitPlatform()
-}
+}*/
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
